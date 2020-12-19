0f3265246277a41e87e8dd74483c5528c1888a2d
Rafael Bucher <rafael.bucher@myskillcamp.com>
2020-12-19 05:27:19 +0100

    countdown timer - frontend mentor challenge

---
+++ script/main.js
@@ -0,0 +1,45 @@
+function getTimeRemaining(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+
+    return {
+        total,
+        days,
+        hours,
+        minutes,
+        seconds
+    };
+}
+
+function initializeClock(id, endtime) {
+    const clock = document.getElementById(id);
+    const daysSpan = clock.querySelector('.rb-countdown__days');
+    const hoursSpan = clock.querySelector('.rb-countdown__hours');
+    const minutesSpan = clock.querySelector('.rb-countdown__minutes');
+    const secondsSpan = clock.querySelector('.rb-countdown__seconds');
+
+    function updateClock() {
+        const t = getTimeRemaining(endtime);
+
+        daysSpan.innerHTML = t.days;
+        hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+        minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+        secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+        if (t.total <= 0) {
+            clearInterval(timeinterval);
+        }
+
+        console.log('SECONDS', secondsSpan);
+        console.log('MINUTES', minutesSpan);
+    }
+
+    updateClock();
+    const timeinterval = setInterval(updateClock, 1000);
+}
+
+const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+initializeClock('countdown', deadline);
